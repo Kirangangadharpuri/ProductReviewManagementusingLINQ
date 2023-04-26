9ccf015babb24de5898ed535281e73d9d3fb70ab
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-26 13:57:25 +0530

    create code to retrive max top 3 records

---
+++ Management.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Management
+    {
+        public void Top3Records(List<ProductReview> ListOfProductReview)
+        {
+            var RecordData = (from Products in ListOfProductReview
+                           orderby Products.Rating descending
+                           select Products).Take(3);
+            foreach (var Record in RecordData)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4}", Record.ProductID, Record.UserID, Record.Rating, Record.Review, Record.IsLike);
+            }
+        }
+    }
+}
